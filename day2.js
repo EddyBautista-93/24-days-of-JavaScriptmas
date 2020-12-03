80ace8611c17a0c78a79504eb74a45477f332585
EddyBautista <bautista.j.eddy@gmail.com>
2020-12-02 19:50:20 -0600

    day 2

--- day2.js
+++ day2.js
@@ -1,1 +1,21 @@
-console.log('hello World');
+function depositProfit(deposit, rate, threshold) {
+
+    // rate is %, so rate/100  * deposit + deposit per year
+    var years = 0;
+    var total = deposit;
+
+    
+    while (total < threshold){
+        years++;
+        total += total*rate/100;
+
+        console.log(total)
+    }
+    
+    return years;
+}
+const deposit = 100;
+        const rate = 20;
+        const threshold = 170;
+        
+        depositProfit(deposit, rate, threshold)
